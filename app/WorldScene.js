6d962aea2a2a192e0d2f435e3e68b2c6d947ebad
oskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2024-05-17 01:08:58 +0700

    migrasi

--- app/WorldScene.js
+++ app/WorldScene.js
@@ -1,44 +1,43 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 export class WorldScene extends THREE.Scene {
-    constructor () {
+  constructor() {
+    super();
+    this.fog = new THREE.Fog(0x0f0e18, 0, 300);
 
-        super();
-        this.fog = new THREE.Fog(0x000000, 0, 500);
+    // Opstions material in the world
+    this.materialA = new THREE.MeshStandardMaterial({ color: 0xffffff });
 
-        // Opstions material in the world
-        this.materialA = new THREE.MeshStandardMaterial({ color: 0x000000 });
+    // Runing setter method
+    this.setLight();
+    this.setFloor();
+  }
 
-        // Runing setter method
-        this.setLight();
-        this.setFloor();
-    }
+  setLight() {
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+    this.add(ambientLight);
 
-    setLight () {
-        const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
-        this.add(ambientLight);
-    
-        const spotlight = new THREE.SpotLight(0xffffff, 0.9, 0, Math.PI / 4, 1);
-        spotlight.position.set(10, 80, 20);
-        spotlight.target.position.set(0, 0, 0);
-    
-        spotlight.castShadow = true;
-    
-        spotlight.shadow.camera.near = 10;
-        spotlight.shadow.camera.far = 100;
-        spotlight.shadow.camera.fov = 30;
-    
-        // spotlight.shadow.bias = -0.0001
-        spotlight.shadow.mapSize.width = 2048;
-        spotlight.shadow.mapSize.height = 2048;
-        this.add(spotlight);
-    }
+    const spotlight = new THREE.SpotLight(0xffffff, 0.9, 0, Math.PI / 4, 1);
+    spotlight.position.set(10, 80, 20);
+    spotlight.target.position.set(0, 0, 0);
 
-    setFloor () {
-        const floorGeometry = new THREE.PlaneGeometry(300, 300, 100, 100);
-        floorGeometry.rotateX(-Math.PI / 2);
-        const floor = new THREE.Mesh(floorGeometry, this.materialA);
-        floor.receiveShadow = true;
-        this.add(floor);
-    }
+    spotlight.castShadow = true;
+
+    spotlight.shadow.camera.near = 10;
+    spotlight.shadow.camera.far = 100;
+    spotlight.shadow.camera.fov = 30;
+
+    // spotlight.shadow.bias = -0.0001
+    spotlight.shadow.mapSize.width = 2048;
+    spotlight.shadow.mapSize.height = 2048;
+    this.add(spotlight);
+  }
+
+  setFloor() {
+    const floorGeometry = new THREE.PlaneGeometry(700, 700, 250, 10);
+    floorGeometry.rotateX(-Math.PI / 2);
+    const floor = new THREE.Mesh(floorGeometry, this.materialA);
+    floor.receiveShadow = true;
+    this.add(floor);
+  }
 }
